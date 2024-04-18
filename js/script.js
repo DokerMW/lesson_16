88ed53c33f09592935c10f94a837144645888eaa
DokerMW <abddoker@gmail.com>
2024-04-18 10:50:32 +0500

    add: complete lesson 16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict'
+
+class First {
+	constructor(){
+
+	}
+	hello(){
+		console.log("Привет я метод родителя!");
+	}
+}
+
+class Second extends First {
+	hello(){
+		super.hello()
+		console.log("А я наследуемый метод!");
+	}
+}
+
+const greetings = new Second();
+
+greetings.hello()
